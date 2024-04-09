a9c75637eb645679eb82c48d86e4164b80403b2d
AlianBlank <wangfj11@foxmail.com>
2024-04-09 19:24:11 +0800

    [增加]1. 增加协程组件

---
+++ Runtime/GameFrameXCoroutineCroppingHelper.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace GameFrameX.Coroutine.Runtime
+{
+    public class GameFrameXCoroutineCroppingHelper : MonoBehaviour
+    {
+        private void Start()
+        {
+            _ = typeof(CoroutineComponent);
+        }
+    }
+}
