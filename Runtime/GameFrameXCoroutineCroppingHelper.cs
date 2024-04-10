79f50cc092f980c884a70547745437ae539e9f28
AlianBlank <wangfj11@foxmail.com>
2024-04-10 18:01:57 +0800

    [增加]1. 增加防裁剪标记

--- Runtime/GameFrameXCoroutineCroppingHelper.cs
+++ Runtime/GameFrameXCoroutineCroppingHelper.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
+using UnityEngine.Scripting;
 
 namespace GameFrameX.Coroutine.Runtime
 {
+    [Preserve]
     public class GameFrameXCoroutineCroppingHelper : MonoBehaviour
     {
+        [Preserve]
         private void Start()
         {
             _ = typeof(CoroutineComponent);
